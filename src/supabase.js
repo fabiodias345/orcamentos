21148002f5991a61f70db23604ee741124a567fc
Fabio <refrihu@uel.br>
2026-04-12 13:45:51 -0300

    acesso

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://mkjznhswwuvtxrnoxmnv.supabase.co'
 const supabaseKey = 'sb_publishable_slpWGID7WQB6RYDH2qaJrQ_WcektktO'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: sessionStorage,
+    persistSession: true,
+  }
+})
